01337a2908a185dd9089542929f632ee8f6aff42
Ziyi Wang <test@test>
2020-07-16 14:44:22 -0400

    search bar minor bug fixed

--- stock-tracker/src/components/SearchBar.js
+++ stock-tracker/src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createSelector } from 'reselect'
 import { useDispatch, useSelector } from 'react-redux'
 import './components.css';
@@ -9,25 +9,28 @@ const SearchBar = () => {
   const dispatch = useDispatch();
   const name = state => state.companyName;
   const symbol = state => state.symbol;
-  const reduxState = useSelector(state => state)
-  const placeholder = createSelector(
+  const placeholderSelector = createSelector(
     name,
     symbol,
     (name, symbol) => (name+'  ('+symbol+')')
   )
+  const placeholder = useSelector(placeholderSelector)
   
-
   const handleSubmit = (e) => {
       e.preventDefault();
       console.log("Search for " + search.toUpperCase() + " submitted.");
       dispatch({ type: 'SEARCH_SUBMITTED', payload: search.toUpperCase()});
-      setSearch('')
+      
   }
+
+  useEffect(()=>{
+    setSearch('')
+  },[placeholder])
 
   return (
     <form onSubmit={handleSubmit} className="wrapper">
       <input className="search-icon" type="image" src="../images/search.png" alt="Submit" />
-      <input placeholder={placeholder(reduxState)}
+      <input placeholder={placeholder}
             type="text" 
             value={search}
             onChange={e => setSearch(e.target.value)}
